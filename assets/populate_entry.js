ef25fe8d1805025c2c10d270a0c1b5d323a3721d
carsten <carsten@vrieswerk.nl>
2009-09-19 08:20:16 -0700

    Fixed right arrow character.

--- assets/populate_entry.js
+++ assets/populate_entry.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
 		var entry_id = jQuery('td:first a', this).attr('title');
 		
 		// Append another cell to this row with -> new link
-		jQuery(this).append('<td><a href="./new/?from-entry=' + entry_id + '">&rarr new</a></td>');
+		jQuery(this).append('<td><a href="./new/?from-entry=' + entry_id + '">&rarr; new</a></td>');
 		
 	});
 	
